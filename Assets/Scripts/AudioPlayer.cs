4fe245105a6be070dd6e5eabbd5b0b0f67b8ef4d
Noa van Bentem <noavanbentem@MacBook-Pro-van-Noa.local>
2018-02-19 15:57:13 +0100

    Audio Walking + AgentControl

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+    private AudioSource walk;
+    private Rigidbody character;
+   
+	// Use this for initialization
+	void Start () {
+        walk = (AudioSource)GetComponent(typeof(AudioSource));
+        character = (Rigidbody)GetComponent(typeof(Rigidbody));
+
+        
+    }
+	// Update is called once per frame
+	void Update () {
+        if (character.velocity.magnitude >= 0.2)
+        {
+            walk.Play();
+            walk.loop = true;
+        }
+		
+	}
+}
