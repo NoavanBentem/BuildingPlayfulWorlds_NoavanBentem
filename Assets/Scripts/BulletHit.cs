005900ca241e3d00ed42385eb7427071cd0befa0
Noa van Bentem <noavanb@gmail.com>
2018-03-03 02:26:26 +0100

    Shooting balls

---
+++ Assets/Scripts/BulletHit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHit : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+
+        GameObject enemy = collision.gameObject;
+        if (enemy.name == "Target")
+        {
+            Destroy(enemy);
+        }
+        Destroy(gameObject); // Destroys bullet on collision
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
